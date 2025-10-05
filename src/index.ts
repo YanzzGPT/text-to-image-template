8b9cd09a403ef22fbbaea7371333fdc386e94293
YanzzGPT <161649497+YanzzGPT@users.noreply.github.com>
2025-10-05 21:52:32 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "Manusia sedang memberi pakan ternak",
     };
 
     const response = await env.AI.run(
